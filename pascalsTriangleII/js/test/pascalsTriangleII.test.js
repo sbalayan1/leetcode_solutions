8b75c210aee2a320e8ff8c30719d1cb04265506f
Sean <balayans2014@yahoo.com>
2023-01-24 14:00:29 -0800

    update tests for pascalsTriangleII

--- pascalsTriangleII/js/test/pascalsTriangleII.test.js
+++ pascalsTriangleII/js/test/pascalsTriangleII.test.js
@@ -1,5 +1,6 @@
-// const { expect } = require('chai')
 const getRow = require('../pascalsTriangleII')
+jest.setTimeout(1000)
+
 
 describe('pascalsTriangle.js', () => {
     describe('getRow()', () => {
@@ -19,8 +20,13 @@ describe('pascalsTriangle.js', () => {
             expect(getRow(4)).toStrictEqual([1, 4, 6, 4, 1])
         })
         
-        test('getRow(30) does not timeout', () => {
+        test('getRow(30) does not timeout and returns [1,30,435,4060,27405,142506,593775,2035800,5852925,14307150,30045015,54627300,86493225,119759850,145422675,155117520,145422675,119759850,86493225,54627300,30045015,14307150,5852925,2035800,593775,142506,27405,4060,435,30,1]', () => {           
+            const start = performance.now()
+            getRow(30)
+            const end = performance.now()
+            expect(end - start).toBeLessThan(5000)
             expect(getRow(30)).toStrictEqual([1,30,435,4060,27405,142506,593775,2035800,5852925,14307150,30045015,54627300,86493225,119759850,145422675,155117520,145422675,119759850,86493225,54627300,30045015,14307150,5852925,2035800,593775,142506,27405,4060,435,30,1])
+            
         })
     })
 })
