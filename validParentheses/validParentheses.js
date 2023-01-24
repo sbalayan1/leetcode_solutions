552608dcbe1ba5588f635c0253ba11aa23ad0f76
Sean <balayans2014@yahoo.com>
2023-01-23 19:04:19 -0800

    validParentheses solutions

---
+++ validParentheses/validParentheses.js
@@ -0,0 +1,37 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+
+function isValid(str) {
+    const pairs = ["()", "[]", "{}"]
+    const obj = {}
+    pairs.forEach(pair => obj[pair[0]] = pair[1])
+    const stack = []
+    for (let i = 0; i<str.length; i++) {
+        if (str[i] in obj) {
+            stack.push(str[i])
+        } else {
+            const lts = stack.pop()
+            if (obj[lts] !== str[i]) return false
+        }
+    }
+
+    return stack.length === 0
+}
---
+++ validParentheses/validParentheses.rb
@@ -0,0 +1,42 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+
+# Example 1:
+
+# Input: s = "()"
+# Output: true
+# Example 2:
+
+# Input: s = "()[]{}"
+# Output: true
+# Example 3:
+
+# Input: s = "(]"
+# Output: false
+
+
+def is_valid(s)
+    hash = {
+        "(": ")",
+        "{": "}",
+        "[": "]"
+    }
+
+    stack = []
+    s.each do |char|
+        if hash.key?(char)
+            stack.push(char)
+        else
+            return false if stack.length == 0
+            lts = stack.pop()
+            return false if hash[lts] != char
+        end
+    end
+
+    return stack.length == 0
+end
